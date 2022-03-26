af3bc1268f6d832058593a82e5ccf62a93d9d3ed
SleepyJake18 <94761866+SleepyJake18@users.noreply.github.com>
2022-03-25 23:41:57 -0400

    got to where I can serve data to view

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,11 +1,17 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 const path = require(`path`);
 // Prevent non logged in users from viewing the homepage
 router.get('/', withAuth, async (req, res) => {
   try {
-    res.render(`dashboard`);
+    let posts = {};
+    //let comments = {};
+    const postData = await Post.findAll({include: [{model:Comment}]});
+    posts = postData.map((post) => post.get({plain:true}));
+    res.render(`dashboard`, {
+      posts
+    });
  //what would we like to show on our homepage?
   } catch (err) {
     res.status(500).json(err);
